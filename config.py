fa0909e6b13a14741f71132e1569b6f7d8500847
nx <dnstzzx@qq.com>
2023-07-31 09:44:59 +0800

    改红队

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config():
         self.serial_port = '/dev/ttyUSB0'
         self.serial_baund_rate = 115200
         self.mock_mode = False   # 虚空调试
-        self.we_are_red = False  # 红蓝队
+        self.we_are_red = True  # 红蓝队
 
 configs = Config()
